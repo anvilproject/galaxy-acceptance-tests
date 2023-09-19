bb6fdcf7fc3f037d242b377d3c3508dbe905259d
Keith Suderman <suderman@jhu.edu>
2023-09-18 20:24:00 -0400

    Add Results.md and a custom reporter to record test results.

---
+++ Results.md
@@ -0,0 +1,6 @@
+# Galaxy acceptance test results
+
+The most current result is at the end of the table.
+
+| Data | Time | Test | Duration | Status |
+|------|------|------|---------|--------|
--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 1, //process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['./tests/reporter.ts']],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */

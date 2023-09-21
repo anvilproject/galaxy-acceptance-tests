3ece6c92c3d97d777b357648de72948425733c81
Keith Suderman <suderman@jhu.edu>
2023-09-21 11:26:16 -0400

    Stash changes before pulling

--- .github/bin/checkin.sh
+++ .github/bin/checkin.sh
@@ -4,8 +4,10 @@ name=$2
 shift 2
 
 git config --global user.name 'GitHub Action'
-git config --global user.email 'action@github.com'    
+git config --global user.email 'action@github.com'  
+git stash  
 git pull
+git stash pop
 
 mkdir -p results/$name
 cp playwright-report/index.html results/$dir/$name.html

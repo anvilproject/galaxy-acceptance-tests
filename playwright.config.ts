73780dd6ecdeef15232c70eaedb3ebfeb8f530bd
Keith Suderman <suderman@jhu.edu>
2025-06-09 20:44:20 -0400

    Remove config for auth from  user.json

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,6 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: "only-on-failure",
-    storageState: '.auth/user.json',    
   },
 
   /* Configure projects for major browsers */
